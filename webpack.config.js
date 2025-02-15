5bc60745c4ae7c9806ac55308ba5c77ee17f3f66
daffadevhosting <endsae83@gmail.com>
2025-02-16 05:27:47 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./assets/js/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "_site/assets/js"),
+    path: path.resolve(__dirname, "assets/js"),
   },
   mode: "development",
   module: {
